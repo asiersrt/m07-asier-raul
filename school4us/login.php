80516f7448a16b6a1bbc597f2ab66a4e8e4f4cf0
asiersrt <asierdaw24@gmail.com>
2023-03-24 09:44:20 +0100

    canvis-24març

--- school4us/login.php
+++ school4us/login.php
@@ -1,32 +1,4 @@
-<?php
-session_start();
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "AbuDrive";
-
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nombre_usuario = $_POST["nombre_usuario"];
-    $contraseña = $_POST["contraseña"];
-
-    $sql = "SELECT * FROM usuarios WHERE nombre_usuario = '$nombre_usuario' AND contraseña = '$contraseña'";
-    $result = mysqli_query($conn, $sql);
-
-    if (mysqli_num_rows($result) == 1) {
-        $row = mysqli_fetch_assoc($result);
-        $_SESSION["usuario_id"] = $row["id_usuario"];
-        $_SESSION["nombre"] = $row["nombre_usuario"];
-        header("location: index.php");
-    } else {
-        $error = "Nombre de usuario o contraseña incorrectos.";
-    }
-}
 ?>
 <!DOCTYPE html>
 <html>
